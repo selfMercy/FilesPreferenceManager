f73440a6e356668a6a27e148971bd91a8797a679
selfMercy <waparabka@gmail.com>
2021-03-10 19:08:33 +0600

    release

---
+++ Sample/SampleFPM/FilesPreferenceManager.cs
@@ -0,0 +1,34 @@
+﻿
+using FilesPreferenceManager;
+using System;
+using System.IO;
+
+namespace SampleFPM
+{
+    class FilesPreferenceManager
+    {
+        static void Main(string[] args)
+        {
+            string sampleDirectory = @"C:\Sample folder";
+            Uri sampleFilesAddress = new Uri("http://test1.ru/preferenceFiles.json");
+
+            FilesValidityHandler validityHandler = new FilesValidityHandler(sampleDirectory, FileValidityMode.Sizing);
+
+            if (validityHandler.BeginValidateFiles(sampleFilesAddress))
+            {
+                FilesDownloadHandler downloadHandler = new FilesDownloadHandler(sampleDirectory, validityHandler.preferenceFiles);
+
+                downloadHandler.DownloadTracker += (s, e) =>
+                {
+                    Console.WriteLine(string.Format($"Status: {e.DownloadStatus}"));
+                    Console.WriteLine(string.Format($"Downloading file: {Path.Combine(e.Directory, e.Name)} | Size: {e.Size} | Percentage: {e.Percentage}"));
+
+                    Console.WriteLine();
+                };
+
+                downloadHandler.BeginDownloadFiles();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Source/generatePreference.php
@@ -0,0 +1,35 @@
+<?php  
+
+$scanned_files = array();
+
+function nav($dir){
+
+	global $scanned_files;
+    $main = array_diff(scandir($dir), array('..', '.'));
+
+    if (!count($main))
+        return;
+
+    foreach($main as $sub) {
+
+        if(is_dir($dir . DIRECTORY_SEPARATOR . $sub)) {
+
+        	nav($dir . '/' . $sub);
+        }
+        else {
+
+        	$scanned = array("NAME" => $sub, "DIRECTORY" => $dir, "SIZE" => filesize($dir . '/' . $sub), "HASH" => hash_file("md5", $dir . '/' . $sub), "ADDRESS" => "http://" . $_SERVER['SERVER_NAME'] . '/' . $dir . '/' . $sub);
+			$scanned_files["FILES"][] = $scanned;
+        }
+    }
+}
+nav('Preference files');
+
+$json = fopen('preferenceFiles.json', 'w');
+
+fwrite($json, json_encode($scanned_files));
+fclose($json);
+
+echo "done";
+
+?>
