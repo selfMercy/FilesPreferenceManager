c50ae10517a643e4d99d5953b4ddf3973377ebc5
selfMercy <waparabka@gmail.com>
2021-03-11 20:55:53 +0600

    Added more description

--- Sample/FilesPreferenceManager/Program.cs
+++ Sample/FilesPreferenceManager/Program.cs
@@ -7,29 +7,34 @@ namespace FilesPreferenceManager
     {
         static void Main(string[] args)
         {
+            // The path to the folder for downloading and checking files.
             string SavePath = @"C:\Sample folder";
+
+            // Link to json file, it is generated using PreferenceGenerator.php
             Uri AddressToJson = new Uri("http://test1.ru/preferenceFiles.json");
 
-            //Hashing is better, but sizing faster than hashing
-            FileValidityMode ValidityMode = FileValidityMode.Hashing;
+            // There are two modes for checking files. (Hashing/Sizing)
+            // The first is more accurate, but the second is much faster.
+            FileValidityMode ValidityMode = FileValidityMode.Hashing; //or FileValidityMode.Sizing;
 
             PreferenceTrackingHandler PreferenceTracker = new PreferenceTrackingHandler();
-            
-            PreferenceTracker.DownloadEngineStatusChanged += (s, e) =>
+
+            PreferenceTracker.DownloadEngineStatusChanged += (s, e) => // Fires when the download state changes.
             {
                 Console.WriteLine(string.Format($"Status: {e.DownloadStatus}"));
             };
 
-            PreferenceTracker.DownloadEngineFileChanged += (s, e) =>
+            PreferenceTracker.DownloadEngineFileChanged += (s, e) => // Fires when the next file starts downloading.
             {
                 Console.WriteLine(string.Format($"Download now: {Path.Combine(e.Directory, e.Name)}"));
             };
 
-            PreferenceTracker.DownloadEngineProgressChanged += (s, e) =>
+            PreferenceTracker.DownloadEngineProgressChanged += (s, e) => // Fires when new bytes are received.
             {
                 Console.WriteLine(string.Format($"Downloaded: {e.DownloadedSize} / {e.Size} | Percentage: {e.Percentage}"));
             };
 
+            // Declaring the main instance of the manager class and initializing the download engine.
             FilesPreferenceManager PreferenceManager = new FilesPreferenceManager(SavePath, AddressToJson, ValidityMode, PreferenceTracker);
             PreferenceManager.InitializeDownloadEngine();
 
