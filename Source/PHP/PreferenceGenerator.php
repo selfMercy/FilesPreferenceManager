a2622215a69a48a9171ddb48491f8b1bbea262d8
selfMercy <68443529+selfMercy@users.noreply.github.com>
2021-03-11 22:02:26 +0600

    Cosmetic changes

--- Source/PHP/PreferenceGenerator.php
+++ Source/PHP/PreferenceGenerator.php
@@ -4,7 +4,7 @@ $scanned_files = array();
 
 function nav($dir){
 
-	global $scanned_files;
+    global $scanned_files;
     $main = array_diff(scandir($dir), array('..', '.'));
 
     if (!count($main))
@@ -12,20 +12,20 @@ function nav($dir){
 
     foreach($main as $sub) {
 
-        if(is_dir($dir . DIRECTORY_SEPARATOR . $sub)) {
+        if(is_dir($dir . '/' . $sub)) {
 
         	nav($dir . '/' . $sub);
         }
         else {
 
         	$scanned = array("NAME" => $sub, "DIRECTORY" => $dir, "SIZE" => filesize($dir . '/' . $sub), "HASH" => hash_file("md5", $dir . '/' . $sub), "ADDRESS" => "http://" . $_SERVER['SERVER_NAME'] . '/' . $dir . '/' . $sub);
-			$scanned_files["FILES"][] = $scanned;
+		$scanned_files["FILES"][] = $scanned;
         }
     }
 }
 nav('Preference files');
 
-$json = fopen('preferenceFiles.json', 'w');
+$json = fopen('PreferenceFiles.json', 'w');
 
 fwrite($json, json_encode($scanned_files));
 fclose($json);
